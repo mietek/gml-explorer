9fbfa63d20b2878ae1d57f3b4bcb8eaec50b28a7
Miëtek Bak <mietek@bak.io>
2015-08-11 16:25:49 +0100

    Add option to output unique attribute keys

--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,84 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy as L
-import Data.List
-import Data.Maybe
+import Data.List (sort)
 import qualified Data.HashSet as S
-import System.Environment
-import System.Exit
-import System.IO
-import Text.XML.Expat.SAX
+import Options.Applicative ((<$>), (<*>), (<>), Parser)
+import qualified Options.Applicative as O
+import Text.XML.Expat.SAX (ParseOptions(..), SAXEvent(..))
+import qualified Text.XML.Expat.SAX as X
+
+
+data Options = Options
+  { optInput   :: FilePath
+  , optCommand :: Command
+  }
+  deriving (Eq, Show)
+
+data Command =
+    Tags
+  | AttrKeys
+  deriving (Eq, Ord, Show)
 
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        [path] -> process path
-        _ -> do
-            hPutStrLn stderr "Usage: gml-explorer <XML file>"
-            exitFailure
+    opts <- O.execParser $
+      O.info (O.helper <*> parseOptions)
+         ( O.header "gml-explorer"
+        <> O.progDesc "Explore an OS GML file"
+        <> O.fullDesc
+         )
+    xml <- L.readFile (optInput opts)
+    let results = process (optCommand opts) xml
+    mapM_ C.putStrLn results
 
-process :: FilePath -> IO ()
-process path = do
-    xml <- L.readFile path
-    let tags = parseTags xml
-    print tags
 
-parseTags :: L.ByteString -> [ByteString]
-parseTags =
-      sort
-    . S.toList
-    . S.fromList
-    . catMaybes
-    . map startElementTag
-    . parse opts
-  where
-    opts = ParseOptions Nothing Nothing
+parseOptions :: Parser Options
+parseOptions =
+  Options <$>
+        parseInput
+    <*> parseCommand
 
-startElementTag :: SAXEvent ByteString ByteString -> Maybe ByteString
-startElementTag (StartElement tag _attrs) = Just tag
-startElementTag _ = Nothing
+parseInput :: Parser FilePath
+parseInput =
+    O.argument O.str
+       ( O.metavar "FILEPATH"
+      <> O.help "File containing OS GML input"
+       )
+
+parseCommand :: Parser Command
+parseCommand =
+    O.subparser
+       ( O.command "tags"
+           (O.info (O.helper <*> O.pure Tags)
+           (O.progDesc "Output unique tags"))
+      <> O.command "attrkeys"
+           (O.info (O.helper <*> O.pure AttrKeys)
+           (O.progDesc "Output unique attribute keys"))
+       )
+
+
+process :: Command -> L.ByteString -> [ByteString]
+process Tags      = unique . stream elementTag
+process AttrKeys  = unique . stream elementAttrKeys
+
+
+unique :: [ByteString] -> [ByteString]
+unique = sort . S.toList . S.fromList
+
+
+stream :: (SAXEvent ByteString ByteString -> [ByteString]) -> L.ByteString -> [ByteString]
+stream fun = concatMap fun . X.parse (ParseOptions Nothing Nothing)
+
+elementTag :: SAXEvent ByteString ByteString -> [ByteString]
+elementTag (StartElement tag _) = [tag]
+elementTag _ = []
+
+elementAttrKeys :: SAXEvent ByteString ByteString -> [ByteString]
+elementAttrKeys (StartElement _ attrs) = map fst attrs
+elementAttrKeys _ = []
